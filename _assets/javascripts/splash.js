b6c8c6be6ae1f92bbf95b566def7d13ac7093572
Matheus Felipe <matbhz@matbhz.local>
2015-07-14 00:27:45 +0200

    Add animated header to home page

---
+++ _assets/javascripts/splash.js
@@ -0,0 +1,58 @@
+var container = document.getElementById('start-splash-bubbles');
+var container_width = container.offsetWidth;
+var container_height = container.offsetHeight;
+
+var sketch = function(s) {
+
+    var time, theta = 0;
+    var frames = 2400,
+        num = 100,
+        num2 = 50;
+    var rs;
+
+    s.setup = function () {
+        s.createCanvas(container_width, container_height);
+        s.smooth(8);
+        s.noStroke();
+        rs = s.random(10000);
+
+        window.onresize = function() { 
+          container_width = container.offsetWidth;
+          container_height = container.offsetHeight;
+          s.createCanvas(container_width, container_height);
+        }
+    }
+
+    s.draw = function() {
+        s.randomSeed(rs);
+        s.background("#58A1CE");
+        time = (s.frameCount % frames) / s.float(frames);
+
+        for (var i = 0; i < num; i++) {
+            drawBubble(i);
+        }
+        theta += s.TWO_PI / frames;
+    }
+
+    function drawBubble(i) {
+        var x = s.random(s.width);
+        var y = s.random(s.height);
+        var sc = s.random(1, 3);
+        var rotation = s.random(-.01, 0.1);
+        var m = s.map(s.sin(theta + (s.TWO_PI / num) * i), -1, 1, .5, 1);
+        var sz = s.random(20, 70) * m;
+        s.push();
+        s.scale(sc);
+        s.translate((x + s.frameCount) % s.width, y % s.height);
+        s.rotate(rotation);
+        s.fill('rgba(255, 255, 255, 1)');
+        if (s.random(1) > .4) s.fill('rgba(55,55,55, 1)');
+        if (s.random(1) > .8) s.fill('rgba(0, 0, 0, 1)');
+        s.ellipse(0, -time * s.height + s.height, sz, sz);
+        s.ellipse(0, -time * s.height, sz, sz);
+        s.pop();
+    }
+
+};
+
+var bubbles = new p5(sketch,'start-splash-bubbles');
